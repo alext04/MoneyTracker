504c1f3b291175bc6f2aa9e0da04d369fde7d24e
alext04 <alexthuruthel04@gmail.com>
2026-06-23 15:25:35 +0530

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Alex Thuruthel on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
